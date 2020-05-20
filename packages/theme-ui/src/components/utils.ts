6cae3b0ea8902f4b50f46536bcd73d3d63366426
Javier Mota <javier.mota.diaz@gmail.com>
2020-05-20 13:28:18 +0200

    Add styleguide command

--- packages/theme-ui/src/components/utils.ts
+++ packages/theme-ui/src/components/utils.ts
@@ -12,5 +12,5 @@ const MRE = /^m[trblxy]?$/;
 export const getMargin = getProps((k) => MRE.test(k));
 export const omitMargin = getProps((k) => !MRE.test(k));
 let idx = 0;
-export const unique: (arg: string) => string = (prefix = "") =>
+export const unique: (arg?: string) => string = (prefix = "") =>
   `${prefix}${idx++}`;
